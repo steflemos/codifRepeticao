988fc62afc95e0498b5808c393affad5a8b44c48
onicksg <nicolas@duel.com.br>
2021-11-24 16:56:27 -0200

    primeiro commit

---
+++ aula.js
@@ -0,0 +1,8 @@
+const diasSemanas = ["domingo","segunda","terça","quarta","quinta","sexta","sabado"]
+console.log(diasSemanas[0]);
+console.log(diasSemanas[1]);
+console.log(diasSemanas[2]);
+console.log(diasSemanas[3]);
+console.log(diasSemanas[4]);
+console.log(diasSemanas[5]);
+console.log(diasSemanas[6]);
